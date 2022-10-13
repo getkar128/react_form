44d8a9d0ce03bbd3c2a72c6b65a39c5c675d3e47
Karthik <getkar128@gmail.com>
2022-10-13 13:30:47 +0300

    Progress Bar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Form from "./components/Form";
+import ProgressBar from "./components/ProgressBar";
 import { FormProvider } from './context/FormContext'
 
 function App() {
   return (
     <FormProvider>
+      <ProgressBar />
       <Form />
     </FormProvider>
   );
